b219c31ae81c940aea70cc0df1f22d1ee139467f
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-06-28 21:04:37 +0530

    Add files via upload

---
+++ pattern4.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=i;j<=i;j++){
+            for(int z=1;z<=j;z++){
+                cout<<i<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
