ceb44ac0b5927bc89cba90fbe61c5486ca99b48b
Reeva Gupta <114238405+reevacodes@users.noreply.github.com>
2025-07-09 00:51:18 +0530

    Add files via upload

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,52 +1,62 @@
-#include <bits/stdc++.h>
+// LEETCODE - 15
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-vector<vector<int>> triplet(int n, vector<int> &arr) {
+vector<vector<int>> threeSum(vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
     vector<vector<int>> ans;
-    sort(arr.begin(), arr.end());
-    for (int i = 0; i < n; i++) {
-        //remove duplicates:
-        if (i != 0 && arr[i] == arr[i - 1]) continue;
 
-        //moving 2 pointers:
+    for (int i = 0; i < n; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;  // skip duplicates
+
         int j = i + 1;
         int k = n - 1;
+
         while (j < k) {
-            int sum = arr[i] + arr[j] + arr[k];
+            int sum = nums[i] + nums[j] + nums[k];
+
             if (sum < 0) {
                 j++;
-            }
-            else if (sum > 0) {
+            } else if (sum > 0) {
                 k--;
-            }
-            else {
-                vector<int> temp = {arr[i], arr[j], arr[k]};
-                ans.push_back(temp);
+            } else {
+                ans.push_back({nums[i], nums[j], nums[k]});
                 j++;
                 k--;
-                //skip the duplicates:
-                while (j < k && arr[j] == arr[j - 1]) j++;
-                while (j < k && arr[k] == arr[k + 1]) k--;
+
+                while (j < k && nums[j] == nums[j - 1]) j++;  // skip duplicates
+                while (j < k && nums[k] == nums[k + 1]) k--;  // skip duplicates
             }
         }
     }
+
     return ans;
 }
 
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
 
-int main()
-{
-    vector<int> arr = { -1, 0, 1, 2, -1, -4};
-    int n = arr.size();
-    vector<vector<int>> ans = triplet(n, arr);
-    for (auto it : ans) {
-        cout << "[";
-        for (auto i : it) {
-            cout << i << " ";
-        }
-        cout << "] ";
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
     }
-    cout << "\n";
+
+    vector<vector<int>> result = threeSum(nums);
+
+    cout << "Triplets that sum to 0:\n";
+    for (auto triplet : result) {
+        for (int num : triplet) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
     return 0;
 }
-
